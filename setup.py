679557d506b3a1d935f522302ce1be43e69eb696
24torky <24torky@gmail.com>
2024-06-24 15:37:56 +0300

    -Adjust version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dependency_needle",
-    version="1.0.0",
+    version="1.0.1",
     description="Dependency injection container",
     author="Your Name",
     author_email="24torky@gmail.com",
