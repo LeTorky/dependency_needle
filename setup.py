3d832d1199ef104a995e0f02c111300431be4be8
24torky <24torky@gmail.com>
2024-06-24 16:55:57 +0300

    -change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dependency_needle",
-    version="1.0.1",
+    version="1.0.2",
     description="Dependency injection container",
     author="Your Name",
     author_email="24torky@gmail.com",
