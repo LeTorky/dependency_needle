f1a93ab2dffd4f5211874c182cf5247b8294bf87
24torky <24torky@gmail.com>
2024-06-26 01:44:04 +0300

    -Adjusted authors name

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('requirements.txt') as f:
 
 setup(
     name="dependency_needle",
-    version="1.1.1",
+    version="1.1.2",
     description="Dependency injection container",
-    author="Your Name",
+    author="Abdelrahman Torky",
     author_email="24torky@gmail.com",
     packages=find_packages(),
     install_requires=requirements
